7c37a6f8ff6dba32f0a3bd03e6af1e01fab66298
Kilig <1775814844@qq.com>
2026-03-07 20:00:29 +0800

    feat: add GitHub Actions workflow for deploying to GitHub Pages; configure Nuxt for static site generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
+  // 必须设置为 static 兼容 GitHub Pages
+  ssr: true, // 或者 false，取决于你是否需要预渲染
+  nitro: {
+    static: true,
+  },
   modules: [
     '@unocss/nuxt',
     '@nuxt/content',
